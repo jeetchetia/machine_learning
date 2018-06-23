aaf075bfdc67cbdec83daedc1916ddb9fcc22ebb
jeetchetia <chiranjeet.chetia@gmail.com>
2018-06-23 10:12:53 -0500

    initial setup

---
+++ RaspberryPi/greeter.py
@@ -0,0 +1,51 @@
+import os
+
+# Greeter is a terminal application that greets old friends warmly,
+#   and remembers new friends.
+
+
+### FUNCTIONS ###
+
+def display_title_bar():
+    # Clears the terminal screen, and displays a title bar.
+    os.system('clear')
+              
+    print("\t**********************************************")
+    print("\t***  Greeter - Hello old and new friends!  ***")
+    print("\t**********************************************")
+    
+def get_user_choice():
+    # Let users know what they can do.
+    print("\n[1] See a list of friends.")
+    print("[2] Tell me about someone new.")
+    print("[q] Quit.")
+    
+    return input("What would you like to do? ")
+    
+
+### MAIN PROGRAM ###
+
+# Set up a loop where users can choose what they'd like to do.
+names = []
+
+choice = ''
+display_title_bar()
+while choice != 'q':    
+    
+    choice = get_user_choice()
+    
+    # Respond to the user's choice.
+    display_title_bar()
+    if choice == '1':
+        print("\nHere are the people I know.\n")
+        for name in names:
+            print(name.title())
+    elif choice == '2':
+        new_name = input("\nPlease tell me this person's name: ")
+        names.append(new_name)
+        print("\nI'm so happy to know %s!\n" % new_name.title())
+    elif choice == 'q':
+        print("\nThanks for playing. Bye.")
+    else:
+        print("\nI didn't understand that choice.\n")
+
